f3d9780eb0f71bbf8bd1f1eafa1098e048abb697
Catalina <150844717+Ctlna@users.noreply.github.com>
2024-07-23 08:12:41 -0500

    feat: :tada: Se inicio el repositorio

---
+++ Dia1/Actividad1/src/actividad1/Actividad1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package actividad1;
+import java.util.*;
+/**
+ *
+ * @author camper
+ */
+public class Actividad1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    Scanner ls = new.Scanner(source:System.in) 
+        
+    }
+    
+}
