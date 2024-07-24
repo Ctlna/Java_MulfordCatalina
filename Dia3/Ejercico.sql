30e8d26f0064e4f894dbc3575445bc0b4ad0e0e4
Catalina <150844717+Ctlna@users.noreply.github.com>
2024-07-24 09:09:55 -0500

    feat: :sparkles: Se iniciaron las tablas

--- Dia3/Ejercico.sql
+++ Dia3/Ejercico.sql
@@ -5,31 +5,15 @@
 create database SIH;
 use SIH;
 
-/*
-1 persona -> muchos hospitales
-1 hospital -> muchos emplea/atiende muchas personas
+create table persona (
+	id int primary key not null,
+    nombre varchar(150) not null,
+    direccion varchar (26)not null,
+    titulo text
+);
 
-persona: 
-- nombre entero
-- dirección
-- titulo
-
-pacientes:
-- edad (calcula de su fecha nacimiento )
-- fecha ingreso hospital o fecha actual
-- asignados específicamente en un pabellón de acuerdo a su diagnóstico
-	(si hay disponibilidad en la capacidad de dicho pabellón)
-
-hospital:
-- tiene departamentos (está vinculado el personal del hospital,)
-- las principales actividades es realizado por personal administrativo, técnico 
-	y de operaciones, quienes atienden a los pacientes.
--  los doctores se encuentran organizados en equipos que tiene un líder que debe ser 
-	doctores asociados y doctores junior que participan en diagnósticos, aunque no 
-    están al cargo de pacientes.
-
- personal:
- - fecha vinculacion
- - salario
-
-*/
+create table paciente(
+	id int primary key not null,
+    id_persona int,
+    foreign key (id_persona) references persona(id)
+);
