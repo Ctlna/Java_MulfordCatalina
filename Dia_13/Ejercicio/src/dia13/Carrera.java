96bc955111d59fdb71264dd6ce45c9e9ad3d2993
Catalina <150844717+Ctlna@users.noreply.github.com>
2024-08-16 07:34:55 -0500

    refactor: :fire: Se eliminaron carpetas innecesarias

--- Dia_13/Ejercicio/src/dia13/Carrera.java
+++ Dia_13/Ejercicio/src/dia13/Carrera.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package dia13;
 
@@ -9,7 +9,13 @@ package dia13;
  * @author camper
  */
 public class Carrera {
-    protected long recorrido = 0;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
     
-    //recorrido=System.currentTimeMillis();
 }
